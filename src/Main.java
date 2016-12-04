a5cf6d6c28dbd6449127c5d11c40fd2286b7b033
Jansey <jansey@freemail.hu>
2016-12-04 12:07:38 +0100

    Első commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.SwingUtilities;
+import model.LoginM;
+import presenter.LoginP;
+import view.LoginV;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				LoginV loginV = new LoginV();
+				loginV.setPresenter(new LoginP( loginV, new LoginM() ));
+				
+			}
+		});
+
+	}
+
+}
---
+++ src/model/LoginM.java
@@ -0,0 +1,17 @@
+package model;
+
+public class LoginM {
+	private String password;
+	
+	public LoginM() {
+		this.password = "password";
+	}
+		     
+    public void setPassword(String pass) {
+    	this.password = pass;
+    }
+     
+    public String getPassword() {
+        return this.password;
+    }
+}
