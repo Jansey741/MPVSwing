8aa473a5c4d4668be6fbc71d605509532a200e76
Loxen <sandor.somodi@gmail.com>
2017-03-06 11:58:58 +0100

    konstans szoveg valtoztatva minimalisan

--- src/presenter/Constants.java
+++ src/presenter/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
 	public static final String C_IGEN_JOHET = "Igen jöhet!";
 	public static final String C_JOHET_A_SZORNYES_TARSAS = "Jöhet a szörnyes Társas?";
-	public static final String C_HELYES_VALASZ = "Helyes válasz!";
+	public static final String C_HELYES_VALASZ = "Ez a helyes válasz!";
 	public static final String C_EZ_IS_HELYES_VALASZ = "Ez is helyes válasz!";
 	
 	
